82b1483c9a7d561befbc8142a4c77845b0081f2f
dressStreet <67256771+Lamzang@users.noreply.github.com>
2022-09-18 00:39:45 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,2 +1,22 @@
 //word list
-const words = [];
+const words = [
+  "rural",
+  "feral",
+  "mango",
+  "affix",
+  "white",
+  "steam",
+  "tumor",
+  "amiss",
+  "baron",
+  "tenor",
+  "ditch",
+  "mirth",
+  "small",
+  "sense",
+  "proud",
+  "fraud",
+  "basis",
+  "fifth",
+  "terse",
+];
